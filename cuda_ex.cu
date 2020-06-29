d3f2391f5c9448042944d38053d5d5e137beac64
wkh <captwk5@gmail.com>
2020-06-29 17:28:32 +0900

    gpu_device

--- cuda_ex.cu
+++ cuda_ex.cu
@@ -1,6 +1,10 @@
 #include "cuda_ex.hpp"
 
 #define N 10000000
+#define MN 16
+
+#define SIZE 21904
+#define RCSIZE 148
 
 __global__ void add_gpu(int n, float *x, float *y, float *z){
     int index = threadIdx.x;
@@ -11,115 +15,183 @@ __global__ void add_gpu(int n, float *x, float *y, float *z){
 }
 
 __host__ void add_cpu(int n, float *x, float *y, float *z){
+
     for(int i = 0; i < n; i++){
         z[i] = x[i] + y[i];
     }
 }
 
-__global__ void MatAdd(float A[N][N], float B[N][N], float C[N][N]){
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    if (i < N && j < N)
-        C[i][j] = A[i][j] + B[i][j];
+__global__ void MatAdd(float *A, float *B, float *C){
+    // threadIdx : 블록 안에 memory idx
+    // blockIdx : 메모리를 담는 단위
+    int COL = blockIdx.x * blockDim.x + threadIdx.x;
+    int ROW = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // printf("BD : %d , BI : %d , TH : %d\n", blockDim.x, blockIdx.x, threadIdx.x);
+    // printf("RBD : %d , RBI : %d , RTH : %d\n", blockDim.y, blockIdx.y, threadIdx.y);
+
+    // float tmpSum = 0;
+
+    // if (ROW < MN && COL < MN){
+    //     // printf("%f\n", A[ROW * MN + COL]);
+
+    //     // for (int i = 0; i < MN; i++) {
+    //         // tmpSum += A[ROW * MN + i] * B[i * MN + COL];
+    //     // }
+    //     C[ROW * MN + COL] = A[ROW * MN + COL] + B[ROW * MN + COL];
+    // }else{
+    //     // printf("??");
+    // }
+    C[ROW * RCSIZE + COL] = A[ROW * RCSIZE + COL] + B[ROW * RCSIZE + COL];
 }
 
 void Cuda_Computing::test(){
     cout << "GPU COUNT : ";
     cout << m_gpu_count << endl;
 
-    int blockSize = 256;
-    int numBlocks = (N + blockSize - 1) / blockSize;
+    for(int i = 0; i < m_gpu_count; i++){
+        cudaGetDeviceProperties( &prop, i );
+        printf( "Name:  %s\n", prop.name );
+        // printf( "Compute capability:  %d.%d\n", prop.major, prop.minor );
+        // printf( "Clock rate:  %d\n", prop.clockRate );
 
-    float *x, *y, *z;
-    cudaMallocManaged(&x, N * sizeof(float));
-    cudaMallocManaged(&y, N * sizeof(float));
-    cudaMallocManaged(&z, N * sizeof(float));
+        printf( "Multiprocessor count:  %d\n", prop.multiProcessorCount );
+        printf( "Shared mem per mp:  %ld\n", prop.sharedMemPerBlock );
+        printf( "Registers per mp:  %d\n", prop.regsPerBlock );
 
-    float *xc, *yc, *zc;
-    xc = new float[N];
-    yc = new float[N];
-    zc = new float[N];
+        printf( "Threads in warp:  %d\n", prop.warpSize );
+        printf( "Max threads per block:  %d\n", prop.maxThreadsPerBlock );
+        printf( "Max thread dimensions:  (%d, %d, %d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2] );
+        printf( "Max grid dimensions:  (%d, %d, %d)\n",prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2] );
 
-    for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-
-        xc[i] = 1.0f;
-        yc[i] = 2.0f;
+        break;
     }
 
     cudaEvent_t start, stop;
     float time;
 
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+
+    // float *x, *y, *z;
+    // cudaMallocManaged(&x, N * sizeof(float));
+    // cudaMallocManaged(&y, N * sizeof(float));
+    // cudaMallocManaged(&z, N * sizeof(float));
+
+    // float *xc, *yc, *zc;
+    // xc = new float[N];
+    // yc = new float[N];
+    // zc = new float[N];
+
+    // for (int i = 0; i < N; i++) {
+        // x[i] = 1.0f;
+        // y[i] = 2.0f;
+
+        // xc[i] = 1.0f;
+        // yc[i] = 2.0f;
+    // }
+
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    cudaEventRecord( start, 0 );
+    // cudaEventRecord( start, 0 );
 
-    add_gpu<<<numBlocks, blockSize>>>(N, x, y, z);
+    // add_gpu<<<numBlocks, blockSize>>>(N, x, y, z);
     
+    // cudaEventRecord( stop, 0 );
+    // cudaEventSynchronize( stop );
+    // cudaEventElapsedTime( &time, start, stop );
+
+    // cudaDeviceSynchronize();
+
+    // cout << "GPU : " << time << endl;
+    // cout << z[0] << " - " << z[N - 1] << endl;
+
+    // cudaEventRecord( start, 0 );
+
+    // add_cpu(N, xc, yc, zc);
+
+    // cudaEventRecord( stop, 0 );
+    // cudaEventSynchronize( stop );
+    // cudaEventElapsedTime( &time, start, stop );
+
+    // cout << "CPU : " << time << endl;
+    // cout << zc[0] << " - " << zc[N - 1] << endl;
+
+    vector<float> hA(SIZE);
+    vector<float> hB(SIZE);
+    vector<float> hC_(SIZE);
+
+    for (int i = 0; i < RCSIZE; i++){
+        for (int j = 0; j < RCSIZE; j++){
+            hA[i * RCSIZE + j] = 1.0f;
+            hB[i * RCSIZE + j] = 2.0f;
+        }
+    }
+
+    CudaArray<float> dA(SIZE);
+    CudaArray<float> dB(SIZE);
+    CudaArray<float> dC(SIZE);
+
+    dA.set(&hA[0], SIZE);
+    dB.set(&hB[0], SIZE);
+
+    dim3 threadsPerBlock(MN, MN); // 32
+    // dim3 threadsPerBlock(1, 1); // 32
+    // dim3 numBlocks_(SIZE / threadsPerBlock.x, SIZE / threadsPerBlock.y);
+    dim3 numBlocks_(512, 512);
+
+    // if (MN * MN > 512){
+    //     cout << SIZE << endl;
+    //     threadsPerBlock.x = 512;
+    //     threadsPerBlock.y = 512;
+    //     numBlocksMat.x = ceil(double(MN) / double(threadsPerBlock.x));
+    //     numBlocksMat.y = ceil(double(MN) / double(threadsPerBlock.y));
+
+    //     cout << numBlocksMat.x << endl;
+    // }
+
+    cudaEventRecord( start, 0 );
+    MatAdd<<<numBlocks_, threadsPerBlock>>>(dA.getData(), dB.getData(), dC.getData());
+
     cudaEventRecord( stop, 0 );
     cudaEventSynchronize( stop );
     cudaEventElapsedTime( &time, start, stop );
 
     cudaDeviceSynchronize();
 
-    cout << "GPU : " << time << endl;
-    cout << z[0] << " - " << z[N - 1] << endl;
+    cout << "GPU Mat : " << time << endl;
+    dC.get(&hC_[0], SIZE);
+
+    cout << hC_[SIZE - 1] << endl;
+
+    float *hC;
+    hC = new float[SIZE];
 
     cudaEventRecord( start, 0 );
-
-    add_cpu(N, xc, yc, zc);
-
+    for(int r = 0; r < RCSIZE; r++){
+        for(int c = 0; c < RCSIZE; c++){
+            hC[r * RCSIZE + c] = hA[r * RCSIZE + c] + hB[r * RCSIZE + c];
+        }
+    }
     cudaEventRecord( stop, 0 );
     cudaEventSynchronize( stop );
     cudaEventElapsedTime( &time, start, stop );
 
-    cout << "CPU : " << time << endl;
-    cout << zc[0] << " - " << zc[N - 1] << endl;
+    cout << "CPU Mat : " << time << endl;
+
+    cout << hC[SIZE - 1] << endl;
 
     cudaEventDestroy( start );
     cudaEventDestroy( stop );
 
-    int mn = 16;
-    int SIZE = mn * mn;
-    vector<float> hA(SIZE);
-    vector<float> hB(SIZE);
+    // cudaFree(x);
+    // cudaFree(y);
+    // cudaFree(z);
 
-    for (int i = 0; i < mn; i++){
-        for (int j = 0; j < mn; j++){
-            hA[i * mn + j] = 1.0f;
-            hB[i * mn + j] = 2.0f;
-        }
-    }
-
-    CudaArray<float> dA(SIZE);
-    CudaArray<float> dB(SIZE);
-
-    dA.set(&hA[0], SIZE);
-    dB.set(&hB[0], SIZE);
-
-    dim3 threadsPerBlock(mn, mn);
-    dim3 numBlocksMat(N / threadsPerBlock.x, N / threadsPerBlock.y);
-    // MatAdd<<<numBlocksMat, threadsPerBlock>>>(A, B, C);
-
-    float *hC;
-    hC = new float[SIZE];
-    for(int r = 0; r < mn; r++){
-        for(int c = 0; c < mn; c++){
-            hC[r * mn + c] = hA[r * mn + c] + hB[r * mn + c];
-        }
-    }
-
-    cout << hC[0] << endl;
-
-    cudaFree(x);
-    cudaFree(y);
-    cudaFree(z);
-
-    delete[] xc;
-    delete[] yc;
-    delete[] zc;
+    // delete[] xc;
+    // delete[] yc;
+    // delete[] zc;
 
     delete[] hC;
 }
--- cuda_ex.hpp
+++ cuda_ex.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdio.h>
 #include "cuda_runtime_api.h"
 
 using namespace std;
@@ -13,6 +14,7 @@ public:
     void test();
 private:
     int m_gpu_count = 0;
+    cudaDeviceProp prop;
 };
 
 template <class T>
@@ -34,6 +36,17 @@ public:
         return end_ - start_;
     }
 
+    // get data
+    const T* getData() const
+    {
+        return start_;
+    }
+
+    T* getData()
+    {
+        return start_;
+    }
+
     void set(const T* src, size_t size){
         size_t min = std::min(size, getSize());
         cudaError_t result = cudaMemcpy(start_, src, min * sizeof(T), cudaMemcpyHostToDevice);
@@ -41,6 +54,18 @@ public:
             throw std::runtime_error("failed to copy to device memory");
         }
     }
+
+    void get(T* dest, size_t size)
+    {
+        size_t min = std::min(size, getSize());
+        cudaError_t result = cudaMemcpy(dest, start_, min * sizeof(T), cudaMemcpyDeviceToHost);
+        if (result != cudaSuccess)
+        {
+            throw std::runtime_error("failed to copy to host memory");
+        }
+    }
+
+    int count = 0;
 
 private:
     void allocate(size_t size){
